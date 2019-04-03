06f191bd365501933f7a4322fd41df44896bddec
Anton Shangareev <shananton2000@gmail.com>
2019-04-04 01:30:58 +0300

    Add type annotations to visit_* functions and to visit() in PrettyPrinter

--- task06/printer.py
+++ task06/printer.py
@@ -15,21 +15,21 @@ class PrettyPrinter(model.ASTNodeVisitor):
     def append(self, s):
         self.program += s
 
-    def visit(self, node, is_statement):
+    def visit(self, node: model.ASTNode, is_statement):
         node.accept(self)
         if is_statement:
             if not self.program.endswith('}'):
                 self.append(';')
             self.append('\n')
 
-    def visit_number(self, number):
+    def visit_number(self, number: model.Number):
         self.append(str(number.value))
 
-    def visit_function(self, function):
+    def visit_function(self, function: model.Function):
         raise NotImplementedError(
             'Functions cannot be present outside of a function definition')
 
-    def visit_function_definition(self, fun_definition):
+    def visit_function_definition(self, fun_definition: model.FunctionDefinition):
         self.append('def {name}({args}) {\n'.format(
             name=fun_definition.name,
             args=', '.join(fun_definition.function.args)))
@@ -39,7 +39,7 @@ class PrettyPrinter(model.ASTNodeVisitor):
 
         self.append('}\n')
 
-    def visit_conditional(self, conditional):
+    def visit_conditional(self, conditional: model.Conditional):
         self.append('if (')
         self.visit(conditional.condition, False)
         self.append(') {\n')
@@ -54,14 +54,14 @@ class PrettyPrinter(model.ASTNodeVisitor):
 
         self.append('}')
 
-    def visit_print(self, print_cmd):
+    def visit_print(self, print_cmd: model.Print):
         self.append('print ')
         self.visit(print_cmd.expr, False)
 
-    def visit_read(self, read_cmd):
+    def visit_read(self, read_cmd: model.Read):
         self.append('read {name}'.format(name=read_cmd.name))
 
-    def visit_function_call(self, fun_call):
+    def visit_function_call(self, fun_call: model.FunctionCall):
         self.visit(fun_call.fun_expr, False)
         self.append('(')
         for i, expr in enumerate(fun_call.args):
@@ -70,10 +70,10 @@ class PrettyPrinter(model.ASTNodeVisitor):
             self.visit(expr, False)
         self.append(')')
 
-    def visit_reference(self, reference):
+    def visit_reference(self, reference: model.Reference):
         self.append(reference.name)
 
-    def visit_binary_operation(self, bin_operation):
+    def visit_binary_operation(self, bin_operation: model.BinaryOperation):
         self.append('(')
         self.visit(bin_operation.lhs, False)
         self.append(')')
@@ -84,7 +84,7 @@ class PrettyPrinter(model.ASTNodeVisitor):
         self.visit(bin_operation.rhs, False)
         self.append(')')
 
-    def visit_unary_operation(self, un_operation):
+    def visit_unary_operation(self, un_operation: model.UnaryOperation):
         self.append(un_operation.op)
 
         self.append('(')
