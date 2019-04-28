8b8b3409dc6561723078519b09afab8a666210df
Anton Shangareev <shananton2000@gmail.com>
2019-04-28 22:32:42 +0300

    Add tests

--- task10/test/TestBasics.hs
+++ task10/test/TestBasics.hs
@@ -6,22 +6,67 @@ import Basics
 main :: IO ()
 main = defaultMain testsBasics
 
+certainty = 10
+
+trim = take certainty
+
+list1 @??= list2 = trim list1 @?= trim list2 
+
 testsBasics :: TestTree
 testsBasics = testGroup "Unit tests for Basics tasks"
     [testCase "head' works on non-empty list" $
         head' [1,2,3] @?= 1
+    
+    , testCase "head' works on infinite list" $
+        head' [1..] @?= 1
 
     , testCase "tail' works on non-empty list too" $
         tail' [1,2,3] @?= [2,3]
 
+    , testCase "tail' works on empty list" $
+        tail' ([] :: [Int]) @?= []
+
+    , testCase "tail' works on infinite list" $
+        tail' [1..] @??= [2..]
+
     , testCase "take' takes 1 element from 3-element list" $
         take' 1 [1,2,3] @?= [1]
+
+    , testCase "take' takes 3 elements from 10-element list" $
+        take' 3 [1..10] @?= [1,2,3]
+
+    , testCase "take' 3 takes all elements from 2-element list" $
+        take' 3 [1,2] @?= [1,2]
+
+    , testCase "take' takes 3 elements from infinite list" $
+        take' 3 [1..] @?= [1,2,3]
+
+    , testCase "take' 0 returns empty list" $
+        take' 0 [1,2,3] @?= []
+
+    , testCase "take' works on empty list" $
+        take' 10 ([] :: [Int]) @?= []
 
     , testCase "drop' drops 1 element from 3-element list" $
         drop' 1 [1,2,3] @?= [2,3]
 
+    , testCase "drop' drops 3 elements from 10-element list" $
+        drop' 3 [1..10] @?= [4..10]
+
+    , testCase "drop' 3 drops all elements from 2-element list" $
+        drop' 3 [1,2] @?= []
+
+    , testCase "drop' drops 3 elements from infinite list" $
+        drop' 3 [1..] @??= [4..]
+
+    , testCase "drop' works on empty list" $
+        drop' 3 ([] :: [Int]) @?= []
+
     , testCase "filter' selects only even numbers from 0 to 10" $
         filter' even [0..10] @?= [0,2..10]
+
+    , testCase "filter' selects only even numbers from the naturals" $
+        filter' even [1..] @??= [2,4..]
 
     , testCase "foldl'' can be used for finding sum of elements" $
         foldl'' (+) 0 [1,2,3] @?= 6
@@ -29,6 +74,15 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     , testCase "concat' works on finite lists as expected" $
         concat' [1,2,3] [4,5,6] @?= [1..6]
 
+    , testCase "concat' works when the second list is infinite" $
+        concat' [1,2,3] [4..] @??= [1..]
+
+    , testCase "concat' works when the first list is infinite" $
+        concat' [1..] [(-1),(-2)..] @??= [1..]
+
     , testCase "quickSort actualy sorts the list" $
         quickSort' [5,2,3,4,1] @?= [1..5]
+
+    , testCase "quickSort' handles duplicates correctly" $
+        quickSort' [5,2,3,4,2,4,3,1,1,5] @?= [1,1,2,2,3,3,4,4,5,5]
     ]
--- task10/test/TestRobots.hs
+++ task10/test/TestRobots.hs
@@ -9,9 +9,65 @@ main = defaultMain testsRobots
 testsRobots :: TestTree
 testsRobots = let
         walter = robot "Walter" 50 50
+        bob = robot "Bob" 50 100
+        scrap = robot "Boop" 777 0
+        weak = robot "Weaky" 1 1
+        dummy = robot "Dummy" 1 120
     in testGroup "Unit tests for Robots task"
         [ testCase "Test for getName" $
             getName walter @?= "Walter"
+        
+        , testCase "Test for getAttack" $
+            getAttack bob @?= 50
+        
+        , testCase "Test for getHealth" $
+            getHealth bob @?= 100
+
+        , testCase "Test for setName" $
+            (getName $ setName "Smith" bob) @?= "Smith"
+        
+        , testCase "Test for setAttack" $
+            (getAttack $ setAttack 1 bob) @?= 1
+
+        , testCase "Test for setHealth" $
+            (getHealth $ setHealth 1 bob) @?= 1
+
         , testCase "Test for printRobot" $
             printRobot walter @?= "Walter, attack: 50, health: 50"
+
+        , testCase "Test for damage" $
+            (getHealth $ damage bob 10) @?= 90
+
+        , testCase "Test for isAlive True" $
+            isAlive bob @?= True
+
+        , testCase "Test for isAlive False" $
+            isAlive scrap @?= False
+
+        , testCase "Alive attacker fights" $
+            (getHealth $ fight bob scrap) @?= (-50)
+
+        , testCase "Dead attacker fights" $
+            (getHealth $ fight scrap bob) @?= 100
+
+        , testCase "Weak, but alive attacker beats a dead one in a three-round fight unharmed" $
+            threeRoundFight weak scrap @?= weak
+
+        , testCase "Weak attacker gets beaten by defender" $
+            threeRoundFight weak bob @?= robot "Bob" 50 99
+
+        , testCase "Attacker beats defender in the first round, remaining unharmed" $
+            threeRoundFight bob weak @?= bob
+
+        , testCase "Attacker beats defender in the third round" $
+            threeRoundFight bob dummy @?= robot "Bob" 50 99
+        
+        , testCase "Winner takes damage twice" $
+            threeRoundFight dummy bob @?= robot "Bob" 50 98
+        
+        , testCase "Test for neueRobotAttak" $
+            (getHealth $ neueRobotAttak dummy) @?= 114
+
+        , testCase "Test survivors" $
+            survivors @?= [robot "Pascal" 4 9, robot "Beep" 7 3]
         ]
