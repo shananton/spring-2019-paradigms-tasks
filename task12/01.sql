c55042dfec7395d2b132e40e3ebf98b8f48c2f12
Anton Shangareev <shananton2000@gmail.com>
2019-05-20 19:34:55 +0300

    Fix query 01

--- task12/01.sql
+++ task12/01.sql
@@ -2,5 +2,5 @@
 -- площади порядке (от большей к меньшей), для стран с одинаковой
 -- площадью порядок лексикографический. (0,25 баллов)
 SELECT Country.Name from Country
-ORDER BY Country.Population DESC, Country.Name ASC
+ORDER BY Country.SurfaceArea DESC, Country.Name ASC
 LIMIT 5;
